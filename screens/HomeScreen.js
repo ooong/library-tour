33ebe39cca6ef06a94e125b2b776dd050d0c0490
Natalie Ung <ung.natalie@gmail.com>
2018-01-19 22:19:53 -0500

    backtoreact

---
+++ screens/HomeScreen.js
@@ -0,0 +1,402 @@
+import React from 'react';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Button, Alert, TextInput
+} from 'react-native';
+import { WebBrowser } from 'expo';
+
+import { MonoText } from '../components/StyledText';
+
+export default class HomeScreen extends React.Component {
+  static navigationOptions = {
+    header: null,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'title',
+      movies: [],
+      movie: '',
+      total: 0,
+      pictures: [],
+      text: 'placeholder',
+      picturesTest: [],
+      mainPicture: {}
+    }
+  }
+
+  // getPicsFromLOCApi() {
+  //   return fetch('http://loc.gov/pictures/search/?q=panda&co=wpapos&fo=json')
+  //     .then((response) => {
+  //       response.json()
+  //     .then((results) => {
+  //       const finalResults = results.results
+  //       this.setState({
+  //         pictures: finalResults
+  //       })
+  //       return results.results
+  //     })
+  //     })
+  //     .catch((error) => {
+  //       console.error(error);
+  //     });
+  // }
+
+componentDidMount() {
+  const pictures = getPicsFromLOCApi()
+  .then(res => {
+    this.setState({
+      pictures: res
+    })
+  })
+  const movies = getMoviesFromApiAsync()
+  .then(res => {
+    this.setState({
+      movies: res
+    })
+  })
+}
+  
+
+  render() {
+
+    return (
+      <View style={styles.container}>
+        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+          <View style={styles.welcomeContainer}>
+            
+          </View>
+
+          <View style={styles.getStartedContainer}>
+          <Text style={styles.navBar}>CATEGORY</Text>
+
+          <TextInput
+          style={{height: 40}}
+          placeholder="Type here to translate!"
+          onChangeText={(text) => this.setState({text})}
+        />
+
+        <Button
+          onPress={() => {
+          this._handleSearchPress()
+          }}
+          title="Press Me"
+          />
+            
+            <Text style={styles.getStartedText}>HELLO</Text>
+            <Text style={styles.getStartedText}>ANOTHER HELLO</Text>
+
+
+
+
+
+
+            {
+              this.state.movies.map(movie => {
+                return (
+                  <Text key={movie.title} style={styles.getStartedText}>{movie.title}</Text>
+                )
+              })
+            }
+
+            {
+              this.state.picturesTest.map(picture => {
+                return (
+                  <View>
+                    <Image
+                  key={picture.pk}
+                  style={{ width: 300, height: 400 }}
+                  source={{ uri: 'http:'+`${picture.image.full}` }}
+                />
+                <Button
+                onPress={() => {
+                  Alert.alert('Correct!');
+                }}
+                title="Correct"
+              />
+  
+              <Button
+              onPress={() => {
+                Alert.alert('Wrong!');
+              }}
+              title="Wrong"
+            />
+
+                  </View>
+                )
+              })
+
+            }
+
+            
+
+            {
+              this.state.pictures.map(picture => {
+                return (
+                  <Image
+                  key={picture.pk}
+                  style={{ width: 300, height: 400 }}
+                  source={{ uri: 'http:'+`${picture.image.full}` }}
+                />
+                )
+              })
+
+            }
+
+            
+
+            
+
+
+            <Text style={styles.getStartedText}>{this.state.movie}</Text>
+            <Text style={styles.getStartedText}>{this.state.title}</Text>
+
+          
+
+            <Image
+              style={{ width: 150, height: 300 }}
+              source={{ uri: 'http://cdn.loc.gov/service/pnp/cph/3b40000/3b48000/3b48800/3b48886r.jpg' }}
+            />
+
+            <Button
+              onPress={() => {
+                Alert.alert('Correct!');
+              }}
+              title="Correct"
+            />
+
+            <Button
+            onPress={() => {
+              Alert.alert('Wrong!');
+            }}
+            title="Wrong"
+          />
+
+
+          </View>
+
+          <View style={styles.helpContainer}>
+            <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
+              <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
+
+        
+      </View>
+    );
+  }
+
+  _maybeRenderDevelopmentModeWarning() {
+    if (__DEV__) {
+      const learnMoreButton = (
+        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
+          Learn more
+        </Text>
+      );
+
+      return (
+        <Text style={styles.developmentModeText}>
+          Development mode is enabled, your app will be slower but you can use useful development
+          tools. {learnMoreButton}
+        </Text>
+      );
+    } else {
+      return (
+        <Text style={styles.developmentModeText}>
+          You are not in development mode, your app will run at full speed.
+        </Text>
+      );
+    }
+  }
+
+  _handleLearnMorePress = () => {
+    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
+  };
+
+  _handleHelpPress = () => {
+    WebBrowser.openBrowserAsync(
+      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
+    );
+  };
+
+  _handleSearchPress = () => {
+    fetch('http://loc.gov/pictures/search/?q=' + `${this.state.text}` + '&fo=json')
+        .then((response) =>  {
+          return response.json()
+        })   
+        .then(resResults => {
+            const results = resResults.results
+            const tenPics = results.slice(9)
+            console.log(
+              'TENPICS HERE', tenPics
+            )
+            this.setState({
+              
+
+              picturesTest: tenPics
+            })
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+    Alert.alert('pressed!')
+  }
+}
+
+function getMoviesFromApiAsync() {
+  return fetch('https://facebook.github.io/react-native/movies.json')
+    .then((response) => response.json())
+    .then((responseJson) => {
+      return responseJson.movies;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+function getPicsFromLOCApi() {
+  return fetch('http://loc.gov/pictures/search/?q=panda&co=wpapos&fo=json')
+    .then((response) => response.json())
+    .then((responseJson) => {
+      return responseJson.results
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+
+
+// function getPicsFromLOCApi() {
+//   return fetch('http://loc.gov/pictures/search/?q=panda&co=wpapos&fo=json')
+//     .then((response) => {
+//       response.json()
+//     .then((results) => {
+//       console.log('RESULTS.results', results.results)
+//       const finalResults = results.results
+//       this.setState({
+//         pictures: finalResults
+//       })
+//       return results.results
+//     })
+//     })
+//     .catch((error) => {
+//       console.error(error);
+//     });
+// }
+
+// function getPicsFromLOCApi() {
+//   return fetch('http://loc.gov/pictures/search/?q=panda&co=wpapos')
+//   .then((responseObject) => {
+//     res.json(responseObject)
+//   })
+//   .then(resJson => {
+//     console.log('resJson', resJson)
+//   })
+// }
+
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  developmentModeText: {
+    marginBottom: 20,
+    color: 'rgba(0,0,0,0.4)',
+    fontSize: 14,
+    lineHeight: 19,
+    textAlign: 'center',
+  },
+  contentContainer: {
+    paddingTop: 30,
+  },
+  welcomeContainer: {
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  welcomeImage: {
+    width: 100,
+    height: 80,
+    resizeMode: 'contain',
+    marginTop: 3,
+    marginLeft: -10,
+  },
+  getStartedContainer: {
+    alignItems: 'center',
+    marginHorizontal: 50,
+  },
+  homeScreenFilename: {
+    marginVertical: 7,
+  },
+  codeHighlightText: {
+    color: 'rgba(96,100,109, 0.8)',
+  },
+  codeHighlightContainer: {
+    backgroundColor: 'rgba(0,0,0,0.05)',
+    borderRadius: 3,
+    paddingHorizontal: 4,
+  },
+  getStartedText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    lineHeight: 24,
+    textAlign: 'center',
+  },
+  tabBarInfoContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    ...Platform.select({
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: { height: -3 },
+        shadowOpacity: 0.1,
+        shadowRadius: 3,
+      },
+      android: {
+        elevation: 20,
+      },
+    }),
+    alignItems: 'center',
+    backgroundColor: '#fbfbfb',
+    paddingVertical: 20,
+  },
+  tabBarInfoText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    textAlign: 'center',
+  },
+  navigationFilename: {
+    marginTop: 5,
+  },
+  navBar: {
+    fontSize: 20,
+    color: 'rgba(96,100,109, 1)',
+    lineHeight: 24,
+    textAlign: 'center',
+  },
+  helpContainer: {
+    marginTop: 15,
+    alignItems: 'center',
+  },
+  helpLink: {
+    paddingVertical: 15,
+  },
+  helpLinkText: {
+    fontSize: 14,
+    color: '#2e78b7',
+  },
+});
